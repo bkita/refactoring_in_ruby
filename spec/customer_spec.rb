bf569e16013b22d6737cfd58d2aab69db043b2c6
Bartosz Kita <bkita@afilias.info>
2016-04-25 10:51:52 +0100

    Added tests for five days

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -11,54 +11,111 @@ describe 'Customer' do
   let(:childrens_movie) { Movie.new('Childrens movie', Movie::CHILDRENS) }
   let(:rental) { Movie.new('Childrens movie', Movie::CHILDRENS) }
 
-  describe 'rents one' do
-    describe 'regular movie' do
+  describe 'Rents one movie for one day' do
+    number_of_days = 1
+
+    describe 'Regular movie' do
       it 'should have one regular movie' do
-        customer.add_rental(rental(regular_movie, 1))
+        customer.add_rental(rental(regular_movie, number_of_days))
         expect(customer.statement).to include 'Regular movie	2'
       end
 
       it 'should show amount owned is 2' do
-        customer.add_rental(rental(regular_movie, 1))
+        customer.add_rental(rental(regular_movie, number_of_days))
         expect(customer.statement).to include 'Amount owed is 2'
       end
 
       it 'should show 1 earned frequent points' do
-        customer.add_rental(rental(regular_movie, 1))
+        customer.add_rental(rental(regular_movie, number_of_days))
         expect(customer.statement).to include 'You earned 1 frequent renter points'
       end
     end
 
-    describe 'new release movie' do
+    describe 'New release movie' do
       it 'should have one new release movie' do
-        customer.add_rental(rental(new_release_movie, 1))
+        customer.add_rental(rental(new_release_movie, number_of_days))
         expect(customer.statement).to include 'New release movie	3'
       end
 
       it 'should show amount owned is 3' do
-        customer.add_rental(rental(new_release_movie, 1))
+        customer.add_rental(rental(new_release_movie, number_of_days))
         expect(customer.statement).to include 'Amount owed is 3'
       end
 
       it 'should show 1 earned frequent points' do
-        customer.add_rental(rental(new_release_movie, 1))
+        customer.add_rental(rental(new_release_movie, number_of_days))
         expect(customer.statement).to include 'You earned 1 frequent renter points'
       end
     end
 
-    describe 'childrens movie' do
+    describe 'Childrens movie' do
       it 'should have one childrens movie' do
-        customer.add_rental(rental(childrens_movie, 1))
+        customer.add_rental(rental(childrens_movie, number_of_days))
         expect(customer.statement).to include 'Childrens movie	1.5'
       end
 
       it 'should show amount owned is 3' do
-        customer.add_rental(rental(childrens_movie, 1))
+        customer.add_rental(rental(childrens_movie, number_of_days))
         expect(customer.statement).to include 'Amount owed is 1.5'
       end
 
       it 'should show 1 earned frequent points' do
-        customer.add_rental(rental(childrens_movie, 1))
+        customer.add_rental(rental(childrens_movie, number_of_days))
+        expect(customer.statement).to include 'You earned 1 frequent renter points'
+      end
+    end
+  end
+
+  describe 'Rents one movie for five days' do
+    number_of_days = 5
+
+    describe 'Regular movie' do
+      it 'should have one regular movie' do
+        customer.add_rental(rental(regular_movie, number_of_days))
+        expect(customer.statement).to include 'Regular movie	6.5'
+      end
+
+      it 'should show amount owned is 6.5' do
+        customer.add_rental(rental(regular_movie, number_of_days))
+        expect(customer.statement).to include 'Amount owed is 6.5'
+      end
+
+      it 'should show 1 earned frequent points' do
+        customer.add_rental(rental(regular_movie, number_of_days))
+        expect(customer.statement).to include 'You earned 1 frequent renter points'
+      end
+    end
+
+    describe 'New release movie' do
+      it 'should have one new release movie' do
+        customer.add_rental(rental(new_release_movie, number_of_days))
+        expect(customer.statement).to include 'New release movie	15'
+      end
+
+      it 'should show amount owned is 15' do
+        customer.add_rental(rental(new_release_movie, number_of_days))
+        expect(customer.statement).to include 'Amount owed is 15'
+      end
+
+      it 'should show 2 (+1 bonus for a two day new release rental) earned frequent points' do
+        customer.add_rental(rental(new_release_movie, number_of_days))
+        expect(customer.statement).to include 'You earned 2 frequent renter points'
+      end
+    end
+
+    describe 'Childrens movie' do
+      it 'should have one childrens movie' do
+        customer.add_rental(rental(childrens_movie, number_of_days))
+        expect(customer.statement).to include 'Childrens movie	4.5'
+      end
+
+      it 'should show amount owned is 4.5' do
+        customer.add_rental(rental(childrens_movie, number_of_days))
+        expect(customer.statement).to include 'Amount owed is 4.5'
+      end
+
+      it 'should show 1 earned frequent points' do
+        customer.add_rental(rental(childrens_movie, number_of_days))
         expect(customer.statement).to include 'You earned 1 frequent renter points'
       end
     end
