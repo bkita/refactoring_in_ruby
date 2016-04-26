886fa0009f742e0201b2095886fb7142e4724a4d
Bartosz Kita <bkita@afilias.info>
2016-04-26 12:01:20 +0100

    Extracted renter points to a method and moved to rental

--- src/rental.rb
+++ src/rental.rb
@@ -19,4 +19,13 @@ class Rental
     end
     this_amount
   end
+
+  def frequent_renter_points
+    frequent_renter_points = 1
+    # add bonus for a two day new release rental
+    if movie.price_code == Movie::NEW_RELEASE && days_rented > 1
+      frequent_renter_points += 1
+    end
+    frequent_renter_points
+  end
 end
