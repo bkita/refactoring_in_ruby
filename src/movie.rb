7820832415feaf069b9d9376e8c85886bbac64e5
Bartosz Kita <bkita@afilias.info>
2016-05-25 14:36:34 +0100

    Refactored movie class to take price object as a param

--- src/movie.rb
+++ src/movie.rb
@@ -3,26 +3,11 @@ require_relative '../src/new_release_price'
 require_relative '../src/childrens_price'
 
 class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
   attr_reader :title
-  attr_reader :price_code
+  attr_reader :price
 
-  def initialize(title, the_price_code)
-    @title, self.price_code = title, the_price_code
-  end
-
-  def price_code=(value)
-    @price_code=value
-    @price = case price_code
-               when REGULAR
-                 RegularPrice.new
-               when NEW_RELEASE
-                 NewReleasePrice.new
-               when CHILDRENS
-                 ChildrensPrice.new
-             end
+  def initialize(title, price)
+    @title, @price = title, price
   end
 
   def charge(days_rented)
@@ -30,6 +15,6 @@ class Movie
   end
 
   def frequent_renter_points(days_rented)
-    price_code == NEW_RELEASE && days_rented > 1 ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
